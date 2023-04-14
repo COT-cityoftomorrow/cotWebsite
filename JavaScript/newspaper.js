042f76940492266a4683f87c630099f6382c4d3c
THON1203 <103455203+THON1203@users.noreply.github.com>
2023-04-14 13:05:21 +0000

    lu

---
+++ JavaScript/newspaper.js
@@ -0,0 +1,4 @@
+const newsletterBtn = document.querySelector("newsletter-btn");
+const newsletterBtnClose = document.querySelector(".newsletter-close-btn");
+const newsletter = document.querySelector(".newsletter");
+
