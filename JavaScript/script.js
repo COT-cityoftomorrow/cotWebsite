267da0ded55141c6e43ec1610ec8e4593fa5d018
lonnie9020 <lonnie.schemann@posteo.de>
2023-04-19 14:05:10 +0200

    @Fr3d33

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,8 @@
-document.onload = () => {
-    var galerieSkript = document.createElement('script');
-    galerieSkript.src = '/JavaScript/jquery-3.6.4.min.js';
-    var galerieSkript = document.createElement('script');
-    galerieSkript.src = '/JavaScript/jquery.cookie.js';
+var galerieSkript = document.createElement('script');
+galerieSkript.src = '/JavaScript/jquery-3.6.4.min.js';
+var galerieSkript = document.createElement('script');
+galerieSkript.src = '/JavaScript/jquery.cookie.js';
+setTimeout(() => {
     $("body").append("<div id='navidiv'></div>");
     $("#navidiv").load("/HTML/Navi.html");
-}
+}, 1000);
