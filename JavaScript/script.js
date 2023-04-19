7b100861fa6d7b7e5d0761577fcb533ad5152a00
lonnie9020 <lonnie.schemann@posteo.de>
2023-04-19 15:04:58 +0200

    @Fr3d33

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("load", () => {
+document.addEventListener("onload", () => {
     $("a[href='" + window.location.pathname + "']").parents("li").addClass("active");
     $("a[href='" + window.location.pathname + "']").parents("li").removeClass("paddingwith");
     $("body").append("<div id='navidiv'></div>");
