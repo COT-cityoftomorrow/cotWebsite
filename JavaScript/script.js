b3d831b902dd9d868a9f3f3ebd1dd5e3f29cbe87
lonnie9020 <lonnie.schemann@posteo.de>
2023-04-19 15:37:17 +0200

    @Fr3d33

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     $("body").append("<div id='navidiv'></div>");
     $("#navidiv").load("/HTML/Navi.html");
-    const newsletterBtn = $(".newsletter-btn");
-    const newsletterBtnClose = $(".newsletter-close-btn");
-    const newsletter = $(".newsletter");
-    const navidiv = $("#navidiv");
     if (newsletterBtn.length != 0) {
+        const newsletterBtn = $(".newsletter-btn");
+        const newsletterBtnClose = $(".newsletter-close-btn");
+        const newsletter = $(".newsletter");
+        const navidiv = $("#navidiv");
 
         $("a[href='" + window.location.pathname + "']").parents("li").addClass("active");
         $("a[href='" + window.location.pathname + "']").parents("li").removeClass("paddingwith");
@@ -59,6 +59,10 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     } else {
         setTimeout(() => {
+            const newsletterBtn = $(".newsletter-btn");
+            const newsletterBtnClose = $(".newsletter-close-btn");
+            const newsletter = $(".newsletter");
+            const navidiv = $("#navidiv");
             console.log({ newsletterBtn, newsletterBtnClose, newsletter, navidiv })
         }, 5000)
     }
