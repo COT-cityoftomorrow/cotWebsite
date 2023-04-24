3f5bf6db53b72fd084bfff3f9002ce2f7221adf6
lonnie9020 <lonnie.schemann@posteo.de>
2023-04-24 18:04:35 +0200

    dartheme verbessert

--- JavaScript/darktheme.js
+++
@@ -1,9 +0,0 @@
-$( ".change" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-        $( "body" ).removeClass( "dark" );
-        $( ".change" ).text( "Light Mode" );
-    } else {
-        $( "body" ).addClass( "dark" );
-        $( ".change" ).text( "Dark Mode" );
-    }
-});
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -75,6 +75,15 @@ var interaveltest = setInterval(() => {
                     }
                 })
             })
+            $( ".change" ).on("click", function() {
+                if( $( "body" ).hasClass( "dark" )) {
+                    $( "body" ).removeClass( "dark" );
+                    $( ".change" ).text( "Light Mode" );
+                } else {
+                    $( "body" ).addClass( "dark" );
+                    $( ".change" ).text( "Dark Mode" );
+                }
+            });
         }
     }, 100);
 });
